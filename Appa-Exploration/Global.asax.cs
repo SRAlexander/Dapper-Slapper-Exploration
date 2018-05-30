7b7ecf4c50dd780635bdea34a3aad5d1e4f70b7f
Scott Alexander <scott.alexander@civica.co.uk>
2018-05-30 16:58:31 +0100

    Simple Injector added with Service layer

--- Appa-Exploration/Global.asax.cs
+++ Appa-Exploration/Global.asax.cs
@@ -6,13 +6,29 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using IServices.Configuration;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
 
 namespace Appa_Exploration
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// </summary>
     public class WebApiApplication : System.Web.HttpApplication
     {
+        /// <summary>
+        /// 
+        /// </summary>
         protected void Application_Start()
         {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            // Register your types, for instance using the scoped lifestyle:
+            // Since the interfaces and models exisit in a service project, we will set them up there...
+            DependencyConfig.Setup(container);
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
---
+++ IServices/Configuration/DependencyConfig.cs
@@ -0,0 +1,20 @@
+﻿using SimpleInjector;
+
+namespace IServices.Configuration
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class DependencyConfig
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="container"></param>
+        public static void Setup(Container container)
+        {
+            //container.Register<Interface, Class>(LifeStyle.);
+        }
+    }
+}
+    
